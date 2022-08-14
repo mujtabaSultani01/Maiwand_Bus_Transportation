34a2e86ee1dc4152b8f051708dbf1c6af9c8e64c
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-08-14 20:27:30 +0430

    routes has been assigned...

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
-
+Route::resource('indexw','hemowebcontroller');
+Route::resource('announcement','AnnouncementWebController');
+Route::resource('gallery','gallerywebcontroller');
+Route::resource('about','aboutuswebcontroller');
+Route::resource('homeform','homecontroller');
+Route::resource('galleryform','gallery');
+Route::resource('announcementform','announcementController');
 
 ?>
