284877df6009c363b3c9c374c90124f952838f67
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-08-15 16:52:04 +0430

    mentioned pages resource path are added...

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -8,4 +8,11 @@ Route::resource('homeform','homecontroller');
 Route::resource('galleryform','gallery');
 Route::resource('announcementform','announcementController');
 
+Route::resource('abouthistory','historycontroller');
+Route::resource('message','messagecontroller');
+Route::resource('aboutusform','aboutuscontroller');
+Route::resource('/driver','drivercontroller');
+Route::resource('/staff','staffcontroller');
+
+
 ?>
