70f285d5bf3c2c373e823d4b61697e73cf5300df
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-08-29 10:49:24 +0430

    login page added...

---
+++ laravel/resources/views/login/login.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]> <html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]> <html class="lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Login</title>
+  <link rel="stylesheet" href="css/style.css">
+  <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body>
+  <form action="index.html" class="login">
+    <h1>Library</h1>
+    <input type="email" name="email" class="login-input" placeholder="Email Address" autofocus>
+    <input type="password" name="password" class="login-input" placeholder="Password">
+    <input type="submit" value="Login" class="login-submit">
+    <p class="login-help"><a href="index.html">Forgot password?</a></p>
+  </form>
+
+  
+</body>
+</html>
