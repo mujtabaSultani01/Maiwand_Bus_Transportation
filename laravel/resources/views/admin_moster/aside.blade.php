8b833066cb360c566060b6726b4da0865f03cfa1
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-08-29 10:48:01 +0430

    aside and head pages are added...

---
+++ laravel/resources/views/admin_moster/aside.blade.php
@@ -0,0 +1,51 @@
+
+    <!--sidebar start-->
+    <aside>
+      <div id="sidebar" class="nav-collapse ">
+        <!-- sidebar menu start-->
+        <ul class="sidebar-menu">
+          <li class="sub-menu">
+            <a href="javascript:;" class="">
+                          <i class="icon_document_alt"></i>
+                          <span>Save data about</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+            <ul class="sub">
+              <li><a class="" href="driver"> Driver Forms</a></li> 
+              <li><a class="" href="passenger">Passenger Form</a></li>
+              <li><a class="" href="staff">Staff Form </a></li>
+            </ul>
+          </li>
+    
+          <li class="sub-menu">
+            <a href="javascript:;" class="">
+                          <i class="icon_documents_alt"></i>
+                          <span>website Pages</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+            <ul class="sub">
+              <li><a class="" href="homeform">Home</a></li>
+              <li><a class="" href="galleryform"><span>Gallery</span></a></li>
+              <li><a class="" href="announcementform">Announcement</a></li>
+              <li><a class="" href="aboutusform">About Us</a></li>
+               <li><a class="" href="abouthistory">About history</a></li>
+            </ul>
+          </li>
+             <li class="sub-menu">
+            <a href="javascript:;" class="">
+                          <i class="icon_documents_alt"></i>
+                          <span>More Updates</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+            <ul class="sub">
+              <li><a class="" href="homea">Edit Phone & Email</a></li>
+             <li><a class="" href="announcementa">Edit company name</a></li>
+             
+            </ul>
+          </li>
+
+        </ul>
+        <!-- sidebar menu end-->
+      </div>
+    </aside>
+    <!--sidebar end-->
---
+++ laravel/resources/views/admin_moster/heade.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Creative - Bootstrap 3 Responsive Admin Template">
+  <meta name="author" content="GeeksLabs">
+  <meta name="keyword" content="Creative, Dashboard, Admin, Template, Theme, Bootstrap, Responsive, Retina, Minimal">
+  <link rel="shortcut icon" href="img/favicon.png">
+
+  <title>Maiwand buss transportation company</title>
+
+  <!-- Bootstrap CSS -->
+  <link href="{!!asset('nice_admin/css/bootstrap.min.css')!!}" rel="stylesheet">
+  <!-- bootstrap theme -->
+  <link href="{!!asset('nice_admin/css/bootstrap-theme.css')!!}" rel="stylesheet">
+  <!--external css-->
+  <!-- font icon -->
+  <link href="{!!asset('nice_admin/css/elegant-icons-style.css')!!}" rel="stylesheet" />
+  <link href="{!!asset('nice_admin/css/font-awesome.min.css')!!}" rel="stylesheet" />
+  <!-- full calendar css-->
+  <link href="{!!asset('nice_admin/assets/fullcalendar/fullcalendar/bootstrap-fullcalendar.css')!!}" rel="stylesheet" />
+  <link href="{!!asset('nice_admin/assets/fullcalendar/fullcalendar/fullcalendar.css')!!}" rel="stylesheet" />
+  <!-- easy pie chart-->
+  <link href="{!!asset('nice_admin/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css')!!}" rel="stylesheet" type="text/css" media="screen" />
+  <!-- owl carousel -->
+  <link rel="stylesheet" href="{!!asset('nice_admin/css/owl.carousel.css')!!}" type="text/css">
+  <link href="{!!asset('nice_admin/css/jquery-jvectormap-1.2.2.css')!!}" rel="stylesheet">
+  <!-- Custom styles -->
+  <link rel="stylesheet" href="{!!asset('nice_admin/css/fullcalendar.css')!!}">
+  <link href="{!!asset('nice_admin/css/widgets.css')!!}" rel="stylesheet">
+  <link href="{!!asset('nice_admin/css/style.css')!!}" rel="stylesheet">
+  <link href="{!!asset('nice_admin/css/style-responsive.css')!!}" rel="stylesheet" />
+  <link href="{!!asset('nice_admin/css/xcharts.min.css')!!}" rel=" stylesheet">
+  <link href="{!!asset('nice_admin/css/jquery-ui-1.10.4.min.css')!!}" rel="stylesheet">
+  <!-- =======================================================
+    Theme Name: NiceAdmin
+    Theme URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+    Author: BootstrapMade
+    Author URL: https://bootstrapmade.com
+  ======================================================= -->
+</head>
+
