494588566a98ea18a8aa57e8c8ab8e4efa0f6534
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-09-06 14:55:50 +0430

    footer and header pages added...

---
+++ laravel/resources/views/moster/footer.blade.php
@@ -0,0 +1,30 @@
+<div class="footer">
+	<div class="container">
+		<div class="footer-main">
+			<h3>New Sugission</h3>
+			<p>Send me a new sugistion about our services </p>
+		    <form>
+		    	<input type="textarea" cols="34"rows="34" value="Enter your Suggistion" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter your email';}">
+		    	<button type="submit">Submit</button>
+		    </form>
+		</div>
+	</div>
+	
+</div>
+<!--footer end here-->
+<!--copy right-->
+</div>
+<div class="copy-rights">
+	<div class="container">
+		<div class="copy-rights-main">
+<a href="#">Home_/</a>
+<a href="#">Contact_/</a>
+<a href="#">Gallery_/</a>
+<a href="#">Contact_/</a>
+<a href="#">Portpolio_/</a>
+		</div>
+	</div>
+</div>
+<!--/copy rights-->
+</body>
+</html>
---
+++ laravel/resources/views/moster/header.blade.php
@@ -0,0 +1,105 @@
+<html>
+<head>
+<title>@yield('title')</title>
+<link href="{!!asset('link/css/bootstrap.css')!!}" rel="stylesheet" type="text/css" media="all">
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="{!!asset('link/js/jquery-1.11.0.min.js')!!}"></script>
+<!-- Custom Theme files -->
+<link href="{!!asset('link/css/style.css')!!}" rel="stylesheet" type="text/css" media="all"/>
+<!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Training Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--Google Fonts-->
+<link href='//fonts.googleapis.com/css?family=Asap:400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Kreon:400,700,300' rel='stylesheet' type='text/css'>
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="{!!asset('link/js/move-top.js')!!}"></script>
+<script type="text/javascript" src="{!!asset('link/js/easing.js')!!}"></script>
+	<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$(".scroll").click(function(event){		
+	 				event.preventDefault();
+					$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+				});
+			});
+	</script>
+<!-- //end-smoth-scrolling -->
+<script src="{!!asset('link/js/bootstrap.min.js')!!}"></script>
+</head>
+<div class="banner">
+	<div class="container">
+		<div class="header">
+			<div class="col-md-7 logo">
+			<center><a href="#"><h1>Maiwnd Transportation Company</h1></a></center>	
+			</div>
+			<div class="col-md-5 details">
+				<div class="addre">
+					<span class="location"> </span>
+					 
+					<div class="clearfix"> </div>
+				</div>
+				<div class="addre ad-para">
+					<span class="tel-ph"> </span>
+					 <div class="detail-para">					
+						<p> Email: <a href="ceng76.sadiqi@gmail.com">@gmail.com</a></p>
+					  </div>
+					  <div class="clearfix"> </div>
+				</div>
+				<div class="addre addres-mes">
+					<span class="mess"> </span>
+					 <div class="detail-para">
+						<p>+93767499442</p>					
+					  </div>
+					<div class="clearfix"> </div>
+				</div>
+			  <div class="clearfix"> </div>
+			</div>
+			<div class="col-md-5 details">
+				<div class="addre">
+					<span class="location"> </span>
+					  
+					<div class="clearfix"> </div>
+				</div>
+				<div class="addre ad-para">
+					<span class="tel-ph"> </span>
+					 
+					  <div class="clearfix"> </div>
+				</div>
+				<div class="addre addres-mes">
+					<span class="mess"> </span>
+					 
+					<div class="clearfix"> </div>
+				</div>
+			  <div class="clearfix"> </div>
+			</div>
+		  <div class="clearfix"> </div>
+		</div>
+		<div class="top-nav">
+			
+	   			<div id="navbar" class="navbar-collapse collapse">
+	              <ul class="nav navbar-nav">
+	    <li class="home"><a href="{!!asset('indexw')!!}">Home</a></li>			  
+		<li class=""><a href="{!!asset('gallery')!!}">Gallery</a></li>
+		<li class=""><a href="{!!asset('announcement')!!}">Announcement</a></li>
+		<li class=""><a href="{!!asset('about')!!}">About Us</a></li>
+		<li class=""><a href="{!!asset('contact')!!}">Contact Us</a></li>	        
+	              </ul>
+	            </div>
+			</div>     
+		 <div class="banner-bottom">
+		 	<div class="bann-para">
+		 	<h2>....................................
+		 		....................................
+		 		....................................
+		 		....................................
+		 	</h2>
+		 	</p>
+		   </div>
+		   <a href="#"><h3>Attention</h3></a>
+		 </div>
+	</div>
+</div>
+<!--header strat here-->
